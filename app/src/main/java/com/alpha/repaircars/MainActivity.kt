57fa4c07245d7cdeaebb49c92ab27e185ed4a83c
WindroseFS <almir.junior445@gmail.com>
2024-10-20 21:09:55 -0300

    Third Commit

--- app/src/main/java/com/alpha/repaircars/MainActivity.kt
+++ app/src/main/java/com/alpha/repaircars/MainActivity.kt
@@ -1,20 +1,38 @@
 package com.alpha.repaircars
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.alpha.repaircars.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        binding.buttonEntrar.setOnClickListener {
+            val usuario = binding.edTextUsuario.text.toString().trim()
+            val senha = binding.edTextSenha.text.toString().trim()
+
+            if (usuario.isEmpty() || senha.isEmpty()) {
+                Toast.makeText(applicationContext, "Digite o usuário e/ou a senha.", Toast.LENGTH_SHORT).show()
+            }else if(usuario.equals("Almir") && senha.equals("pass")) {
+                Toast.makeText(applicationContext, "Obrigado por testar, $usuario.", Toast.LENGTH_LONG).show()
+            }else{
+                Toast.makeText(applicationContext, "Usuário e/ou senha inválidos.", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
